e146f8e138bf7fd8013509f47a2a6fc64cce7f54
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-03-17 11:08:14 -0700

    adding test case and implementation for one number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,11 +1,17 @@
 def add(str_nums)
   return 0 if str_nums.empty?
+  numbers = str_nums.split(",").map(&:to_i)
+  numbers.reduce(0,:+)
+
 end
 
 RSpec.describe 'StringCalculator' do
   context "#add" do  
     it "return 0 for empty string" do
      expect(add("")).to eq(0)
+    end
+    it "return the number itself for a single number" do
+    expect(add("1")).to eq(1)
     end
   end
 end
