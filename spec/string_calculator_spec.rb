8df29931e6a4b0847beb33013c1e8fdab618c263
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-03-18 02:06:56 -0700

    Separated the functinality to a separte ruby class

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,31 +1,4 @@
-class StringCalculator
-  def self.add(str_nums)
-    return 0 if str_nums.empty?
-    delimiter = extract_delimiter(str_nums)
-    numbers = extract_numbers(str_nums, delimiter)
-    check_negatives(numbers)
-    numbers.reduce(0,:+)
-  end
-
-  def self.extract_delimiter(str_nums)
-    delimiter = ","
-    if str_nums.start_with?("//")
-      delimiter = str_nums[2]
-      str_nums = str_nums.split("\n", 2)[1]
-    end
-    delimiter
-  end
-
-  def self.extract_numbers(str_nums, delimiter)
-    str_nums.gsub!(/\n/, delimiter)
-    str_nums.split(delimiter).map(&:to_i)
-  end
-
-  def self.check_negatives(numbers)
-    negatives = numbers.select { |num| num < 0 }
-    raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
-  end
-end
+require "string_calculator"
 
 RSpec.describe 'StringCalculator' do
   context "#add" do  
