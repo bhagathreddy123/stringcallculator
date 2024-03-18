9e653cc12637c465a5cba441d7a6bbeb6d1b68a3
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-03-18 01:37:02 -0700

    Allow the add method to handle new lines between numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,8 +1,23 @@
 class StringCalculator
   def self.add(str_nums)
     return 0 if str_nums.empty?
-    numbers = str_nums.split(",").map(&:to_i)
+    delimiter = extract_delimiter(str_nums)
+    numbers = extract_numbers(str_nums, delimiter)
     numbers.reduce(0,:+)
+  end
+
+  def self.extract_delimiter(str_nums)
+    delimiter = ","
+    if str_nums.start_with?("//")
+      delimiter = str_nums[2]
+      str_nums = str_nums.split("\n", 2)[1]
+    end
+    delimiter
+  end
+
+  def self.extract_numbers(str_nums, delimiter)
+    str_nums.gsub!(/\n/, delimiter)
+    str_nums.split(delimiter).map(&:to_i)
   end
 end
 
@@ -18,6 +33,10 @@ RSpec.describe 'StringCalculator' do
 
     it "returns the sum of numbers for comma separated numbers" do
       expect(StringCalculator.add("1,4")).to eq(5)
+    end
+
+    it "returns the sum of the numbers for numbers separated by new lines" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
 
   end
