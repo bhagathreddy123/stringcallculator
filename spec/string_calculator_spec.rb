7a9633fda1704d6149ea4a1d3ecd930dd9602669
bhagathreddy123 <katta_reddy04@yahoo.com>
2024-03-17 22:45:34 -0700

    added a class and updated as class method and modified test cases

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,22 +1,23 @@
-def add(str_nums)
-  return 0 if str_nums.empty?
-  numbers = str_nums.split(",").map(&:to_i)
-  numbers.reduce(0,:+)
-
+class StringCalculator
+  def self.add(str_nums)
+    return 0 if str_nums.empty?
+    numbers = str_nums.split(",").map(&:to_i)
+    numbers.reduce(0,:+)
+  end
 end
 
 RSpec.describe 'StringCalculator' do
   context "#add" do  
     it "return 0 for empty string" do
-     expect(add("")).to eq(0)
+     expect(StringCalculator.add("")).to eq(0)
     end
 
     it "return the number itself for a single number" do
-      expect(add("1")).to eq(1)
+      expect(StringCalculator.add("1")).to eq(1)
     end
 
     it "returns the sum of numbers for comma separated numbers" do
-      expect(add("1,4")).to eq(5)
+      expect(StringCalculator.add("1,4")).to eq(5)
     end
 
   end
